6d7388a9b346b4ccef1f80527f294b059ddaf467
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-06-28 22:06:12 +1200

    added functions to add and delete a task

---
+++ client/apiClient.js
@@ -0,0 +1,43 @@
+import request from 'superagent'
+
+export {
+  getAllTasks,
+  addTask,
+  updateTask,
+  deleteTask
+}
+
+function getAllTasks() {
+  return request
+    .get('/api/v1/tasks')
+    .then(resp => {
+      return resp.body
+    })
+    .catch(err => {
+      console.error(err)
+    })
+}
+
+function addTask(task) {
+  return request
+    .post('api/v1/tasks')
+    .send(task)
+    .catch(err => {
+      console.error(err)
+    })
+}
+
+function updateTask (task) {
+  return request.put(`api/v1/task`)
+    .send(task)
+    .catch(err => {
+      throw Error('Cannot PUT a Post!')
+    })
+}
+
+function deleteTask (taskId) {
+  return request.del('api/v1/tasks').send({ id: taskId })
+  .catch(err => {
+    console.error(err)
+  })
+}
--- client/components/Task.jsx
+++ client/components/Task.jsx
@@ -1,45 +1,90 @@
 import React from 'react'
+import { getAllTasks, addTask, updateTask, deleteTask } from '../apiClient';
 
 class Task extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      tasks: [
-        {
-          id: 1,
-          task: 'task 1'
-        },
-        {
-          id: 2,
-          task: 'task 2'
-        }
-      ]
+      tasks: [],
+      task: '',
+      error: null,
+      id: null
     }
+    this.addNewTask = this.addNewTask.bind(this)
+    this.updateTask = this.updateTask.bind(this)
+    this.editTask = this.editTask.bind(this)
   }
 
-  editTask(){
+  componentDidMount() {
+    getAllTasks()
+      .then(tasks => {
+        this.setState({ tasks })
+      })
+  }
+
+  addNewTask(e) {
+    e.preventDefault()
+    this.setState({ error: null })
+    const newTask = { task: this.state.task }
+    addTask(newTask)
+      .then(() => {
+        getAllTasks()
+          .then(tasks => {
+            this.setState({ tasks })
+          })
+      })
+      .catch(err => this.setState({ errorMessage: err.message }))
+  }
+
+  editTask(e) {
+    e.preventDefault()
+    this.setState({ error: null })
+
+    updateTask(this.state.task)
+      .catch(err => this.setState({ error: err.message }))
+  }
+
+  deleteThisTask(task) {
+    // e.preventDefault()
+    this.setState({ error: null })
+    //const id = this.props.id
+    deleteTask(task.id)
+      .then(() => {
+        getAllTasks()
+          .then(tasks => {
+            this.setState({ tasks })
+          })
+      })
 
   }
 
-  deleteTask() {
-
+  updateTask(event) {
+    this.setState({
+      task: event.target.value.substr(0, 50)
+    })
   }
+
 
   render() {
     return (
       <div className="task">
         <h2>Tasks of the day</h2>
-        <input type="text" placeholder="Type task here" name="task" id="task" />
-        <input type="submit" id="taskSubmit" value="Add task" />
+        <form onSubmit={this.addNewTask}>
+          <input type="text" placeholder="Type task here" name="task" id="task" onChange={this.updateTask.bind(this)} />
+          <input type="submit" id="taskSubmit" value="Add task" />
+        </form>
         <ul>
           {this.state.tasks.map(task => {
-            return <li key={task.id}>{task.task} <input type="submit" id="editTask" value="Edit" /> <input type="submit" id="deleteTask" value="Delete" /></li>
+            return <li key={task.id}>{task.task}
+              <input type="submit" id="editTask" value="Edit" id="editTask" onClick={this.editTask.bind(this, task)} />
+              <input type="submit" value="Delete" id="deleteTask" onClick={this.deleteThisTask.bind(this, task)} /></li>
           })}
         </ul>
       </div>
     )
   }
 
-} 
+}
+
 
 export default Task
